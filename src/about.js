e72a72898fb6b299c80756b0728d38bb0de5bbe6
Tyson Robinson <TyRobinson4@shaw.ca>
2023-06-01 11:46:06 -0500

    updates

---
+++ src/about.js
@@ -0,0 +1,2 @@
+import "./global-style.css";
+import "./home-style.css";
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<React.StrictMode></React.StrictMode>);
+
+// import "./about-style.css";
+// import "./contact-style.css";
+import "./global-style.css";
+import "./home-style.css";
+// import "./qualifications-style.css";
+// import "./work-samples-style.css";
